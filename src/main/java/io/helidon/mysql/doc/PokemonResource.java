950dcbb09f55a565c74ebe712447853b240d4a02
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2023-05-17 16:10:10 +0300

    Rework pokemons

--- src/main/java/io/helidon/mysql/doc/PokemonResource.java
+++ src/main/java/io/helidon/mysql/doc/PokemonResource.java
@@ -5,12 +5,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Objects;
 
+import com.mysql.cj.xdevapi.Result;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @ApplicationScoped
@@ -35,8 +34,32 @@ public class PokemonResource {
     @Path("/{limit}")
     @Produces(MediaType.APPLICATION_JSON)
     public String listLimit(@PathParam("limit") Integer limit ) throws Exception{
-        ArrayList<Object> pokemons = pokemonService.getPokemons(!Objects.isNull(limit) ? limit : defaultLimit );
+        ArrayList<Object> pokemons = pokemonService
+                .getPokemons(!Objects.isNull(limit) ? limit : defaultLimit );
         return getResult(pokemons);
+    }
+
+    @POST
+    public long addPokemon(JsonObject jsonObject){
+        return pokemonService
+                .addPokemon(jsonObject.toString())
+                .getAffectedItemsCount();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public long removePokemon(@PathParam("id") String id){
+        return pokemonService
+                .removePokemon(id)
+                .getAffectedItemsCount();
+    }
+
+    @PUT
+    @Path("/{id}/{name}")
+    public long  updatePokemon(@PathParam("id") String id, @PathParam("name") String name){
+        return pokemonService
+                .updatePokemonName(id, name)
+                .getAffectedItemsCount();
     }
 
     private static String getResult(List<?> pokemons){
--- src/main/java/io/helidon/mysql/doc/PokemonService.java
+++ src/main/java/io/helidon/mysql/doc/PokemonService.java
@@ -1,21 +1,14 @@
 package io.helidon.mysql.doc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.mysql.cj.xdevapi.Session;
-import com.mysql.cj.xdevapi.Client;
-import com.mysql.cj.xdevapi.ClientFactory;
-import com.mysql.cj.xdevapi.Collection;
-import com.mysql.cj.xdevapi.DbDoc;
-import com.mysql.cj.xdevapi.DocResult;
-import com.mysql.cj.xdevapi.Schema;
+import com.mysql.cj.xdevapi.*;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class PokemonService {
@@ -41,21 +34,58 @@ public class PokemonService {
     }
 
     public ArrayList<Object> getPokemons(Integer limit) throws Exception {
+        return callInSession(col -> {
+            DocResult result = col.find().limit(limit).execute();
+            return processResults(result.fetchAll());
+        });
+    }
+
+    public AddResult addPokemon(String json) {
+        return callInSession(col -> col.add(json).execute()
+        );
+    }
+
+    public Result removePokemon(String id) {
+
+        return callInSession(col -> col.removeOne(id)
+        );
+    }
+
+    public Result updatePokemonName(String id, String name) {
+        return callInSession(col -> {
+                    ModifyStatement modifyStatement = col.modify("_id='" + id + "'").set("name", name);
+                    return modifyStatement.execute();
+                }
+        );
+    }
+
+    private ArrayList<Object> processResults(List<DbDoc> docs) {
+        ArrayList<Object> result = new ArrayList<>();
+        for (DbDoc doc : docs) {
+            try {
+                result.add(objectMapper.readTree(doc.toString()));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+    //Helper: run code in a Session, using the collection.
+    private <R> R callInSession(CallInSession<R> caller) {
         Session session = cli.getSession();
         Schema schema = session.getSchema(this.schema);
         Collection col = schema.getCollection(this.collection);
-        DocResult result = col.find().limit(limit).execute();
-        ArrayList<Object> docs = processResults(result.fetchAll());
+
+        R result = caller.call(col);
+
         session.close();
-        return docs;
+        return result;
     }
 
-    private ArrayList<Object> processResults(List<DbDoc> docs) throws JsonProcessingException {
-        ArrayList<Object> result = new ArrayList<>();
-        for (DbDoc doc : docs) {
-            result.add(objectMapper.readTree(doc.toString()));
-        }
-        return result;
+    @FunctionalInterface
+    private interface CallInSession<R> {
+        R call(Collection collection);
     }
 
 }
